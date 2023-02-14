dbc072ca4e3b883f21bb9b2ab07ec540306dcfdc
annieeeeeeeeeeeee <annieou000@gmail.com>
2023-02-14 10:16:28 -0500

    6

--- vite-project/main.js
+++ vite-project/main.js
@@ -5,6 +5,7 @@ const DOMSelectors = {
   emojiDisplay: document.getElementById("picture-display"),
   userInput: document.getElementById("userInput"),
   form: document.getElementById("answer-box"),
+  score: document.getElementById("score-number"),
 };
 
 /* function HTML(emojis) {
@@ -24,33 +25,60 @@ const DOMSelectors = {
 score();
  */
 
+let score = 0;
 let question = Math.floor(Math.random() * questions.length);
 let emojiQ = questions[question].emojis;
 let answer = questions[question].answer;
 
-DOMSelectors.emojiDisplay.insertAdjacentHTML(
-  "afterbegin",
-  `
+function start() {
+  next();
+  //genQ();
+  checkAnswer();
+  questions.splice(question, 1);
+}
+
+start();
+
+function genQ() {
+  DOMSelectors.emojiDisplay.insertAdjacentHTML(
+    "afterbegin",
+    `
     <p id="emojis">${emojiQ}</p>
     `
-);
+  );
+}
 
 //if (user input === answer) , score ++
 
-let score = 0;
 function checkAnswer() {
   const userInput = DOMSelectors.userInput.value;
   console.log(userInput);
 
   if (userInput === answer) {
     score++;
+    console.log("correct!");
   } else {
     console.log("wrong");
   }
   console.log(score);
 }
 
+function next() {
+  DOMSelectors.userInput.value = "";
+  DOMSelectors.emojiDisplay.innerHTML = "";
+  genQ();
+  question.forEach((e) =>
+    DOMSelectors.emojiDisplay.insertAdjacentHTML("afterbegin", ``)
+  );
+}
+
+function updateScore() {
+  DOMSelectors.score.textContent = `${score}`;
+}
+
 DOMSelectors.form.addEventListener("submit", function (e) {
   e.preventDefault();
   checkAnswer();
+  next();
+  updateScore();
 });
